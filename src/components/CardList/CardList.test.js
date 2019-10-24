1208bd64d2bfda3d8792e233b3583f5aa407f460
Slava <ucheba00@gmail.com>
2019-10-24 16:19:44 -0600

    add snapshots for cardlist and card components

---
+++ src/components/CardList/CardList.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import CardList from './CardList.jsx';
+
+test(`CardList's snapshot`, () => {
+  const tree = renderer
+    .create(
+        <CardList
+          cards={[
+            {
+              id: 1,
+              src: `img`,
+              price: `10`,
+              rating: `10`,
+              name: `Beautiful`,
+              type: `Private`
+            }
+          ]}
+          onCardHeaderClick={jest.fn()}
+        />
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
