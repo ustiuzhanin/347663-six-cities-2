2cbc982d2944bc198d823e0c02c4f14c8d441c62
Slava <ucheba00@gmail.com>
2021-02-14 21:06:50 -0700

    update profile page

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -16,22 +16,28 @@ const Profile = (props) => {
   return (
     <>
       <Header />
-      <section className="profile">
-        <div className="dd">
-          <pre>{JSON.stringify(userAuth, null, 4)}</pre>
-        </div>
+      <section className="profile container">
         {user && (
-          <div className="property__avatar-wrapper user__avatar-wrapper property__avatar-wrapper--pro">
-            <img
-              className="property__avatar user__avatar"
-              src={avatar_url}
-              width="74"
-              height="74"
-              alt="Host avatar"
-            ></img>
-            <span className="property__user-name">{name}</span>
-            {is_pro && <span className="property__user-status">{is_pro}</span>}
-            <span className="property__user-name">{email}</span>
+          <>
+            <div className="property__avatar-wrapper user__avatar-wrapper property__avatar-wrapper--pro">
+              <img
+                className="property__avatar user__avatar"
+                src={avatar_url}
+                width="74"
+                height="74"
+                alt="Host avatar"
+              ></img>
+            </div>
+            <p className="property__user-status">
+              {is_pro ? "Pro" : "Not Pro"}
+            </p>
+
+            <p>
+              Name: <span className="property__user-name">{name}</span>
+            </p>
+            <p>
+              Email: <span className="property__user-name">{email}</span>
+            </p>
             <div className="offers">
               <h2>Your listings</h2>
               <ul>
@@ -52,7 +58,7 @@ const Profile = (props) => {
                 )}
               </ul>
             </div>
-          </div>
+          </>
         )}
       </section>
     </>
