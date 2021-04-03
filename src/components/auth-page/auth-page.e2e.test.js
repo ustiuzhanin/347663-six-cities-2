6c9b970e44bcc3549bd9f2eec024f966911abcbe
Slava <ucheba00@gmail.com>
2021-04-03 10:41:03 -0600

    add e2e tests form auth-page component

---
+++ src/components/auth-page/auth-page.e2e.test.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { AuthPage } from "./auth-page.jsx";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe(`AuthModal component testing`, () => {
+  it(`checks if form submit calls requestLogin`, () => {
+    const requestLogin = jest.fn();
+    const authPage = shallow(
+      <AuthPage
+        requestSignUp={jest.fn()}
+        requestLogin={requestLogin}
+        isAuthorizationRequired
+        method="Login"
+      />
+    );
+
+    const form = authPage.find(`.login__form`);
+
+    form.simulate(`submit`, { preventDefault: () => {} });
+    expect(requestLogin).toHaveBeenCalledTimes(1);
+  });
+
+  it(`checks if form submit calls requestSignUp`, () => {
+    const requestSignUp = jest.fn();
+    const authPage = shallow(
+      <AuthPage
+        requestSignUp={requestSignUp}
+        requestLogin={jest.fn()}
+        isAuthorizationRequired
+        method="Sign up"
+      />
+    );
+
+    const form = authPage.find(`.login__form`);
+
+    form.simulate(`submit`, { preventDefault: () => {} });
+    expect(requestSignUp).toHaveBeenCalledTimes(1);
+  });
+});
