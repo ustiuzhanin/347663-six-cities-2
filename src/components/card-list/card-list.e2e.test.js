cddcb42bedea2bcdbd5955b92f1a3c2c8ff5b980
Slava <ucheba00@gmail.com>
2021-04-03 16:18:28 -0600

    add e2e tests for city-list component

--- src/components/card-list/card-list.e2e.test.js
+++ src/components/card-list/card-list.e2e.test.js
@@ -7,7 +7,7 @@ import { offer as MockOffer } from "../../mocks/single-offer";
 Enzyme.configure({ adapter: new Adapter() });
 
 describe(`AuthModal component testing`, () => {
-  it(`checks if form submit calls requestLogin`, () => {
+  it(`checks if changeSortingType works as expected `, () => {
     const changeSortingType = jest.fn();
     const cardList = shallow(
       <CardList
@@ -19,12 +19,16 @@ describe(`AuthModal component testing`, () => {
       />
     );
 
-    const form = cardList.find(`#popular`);
+    const sortingOption = cardList.find(`#popular`);
 
-    form.simulate(`click`, { target: { id: "popular", innerText: "Popular" } });
+    sortingOption.simulate(`click`, {
+      target: { id: "popular", innerText: "Popular" },
+    });
     expect(changeSortingType).toHaveBeenCalledTimes(0);
 
-    form.simulate(`click`, { target: { id: "price", innerText: "Price" } });
+    sortingOption.simulate(`click`, {
+      target: { id: "price", innerText: "Price" },
+    });
     expect(changeSortingType).toHaveBeenCalledTimes(1);
     expect(changeSortingType).toHaveBeenCalledWith({
       type: "price",
---
+++ src/components/city-list/city-list.e2e.test.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { CityList } from "./city-list.jsx";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const mockCity = {
+  _id: "6005f36637751d35c8503011",
+  name: `Dusseldorf`,
+  location: { latitude: 51.225402, longitude: 6.776314, zoom: 13 },
+};
+
+describe(`AuthModal component testing`, () => {
+  it(`checks if form submit calls requestLogin`, () => {
+    const onCityLinkClick = jest.fn();
+    const cityList = shallow(
+      <CityList
+        activeCity={`Dusseldorf`}
+        listOfCities={[mockCity]}
+        onCityLinkClick={onCityLinkClick}
+        loadCityList={jest.fn()}
+      />
+    );
+
+    const city = cityList.find(`#Dusseldorf`);
+
+    city.simulate(`click`, {
+      preventDefault: () => {},
+      currentTarget: { id: "Dusseldorf" },
+    });
+    expect(onCityLinkClick).toHaveBeenCalledTimes(0);
+
+    city.simulate(`click`, {
+      preventDefault: () => {},
+      currentTarget: { id: "Paris" },
+    });
+    expect(onCityLinkClick).toHaveBeenCalledTimes(1);
+    expect(onCityLinkClick).toHaveBeenCalledWith("Paris");
+  });
+});
